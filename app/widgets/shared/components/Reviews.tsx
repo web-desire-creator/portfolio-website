8a852fbb16f4c0fd5a6b15209dea17c638e4e3cc
Bilal <sheikhb885@gmail.com>
2023-03-27 23:15:21 +0500

    done

--- app/widgets/shared/components/Reviews.tsx
+++ app/widgets/shared/components/Reviews.tsx
@@ -8,10 +8,10 @@ const Reviews = () => (
         <>
             <div className='text-3xl py-6 font-semibold text-[#7562E0]'>Reviews:</div>
         <div className='flex flex-col sm:flex-row gap-y-10 sm:gap-x-10 items-stretch'>
-            <Client title={'jochenbessemans'} desc={'\"This seller responds very quickly and does what you ask for. His input and suggestions in every step added value. One of my best Fiverr experiences so far!\"'} />
-            <Client title={'gailforce12'} desc={'\"The communication and revisions were dealt with quickly and to great effect. Will use again and can definitely recommend.\"'}/>
-            <Client title={'networkislam'} desc={'\"Thanks for such a quick reponse\"'}/>
-            <Client title={'abgcompu'} desc={'\"Excellent and fast. Will definitely hire again.\"'}/>
+            <Client title={'jochenbessemans'} desc={'&PrimeThis seller responds very quickly and does what you ask for. His input and suggestions in every step added value. One of my best Fiverr experiences so far!&Prime'} />
+            <Client title={'gailforce12'} desc={'&PrimeThe communication and revisions were dealt with quickly and to great effect. Will use again and can definitely recommend.&Prime'}/>
+            <Client title={'networkislam'} desc={'&PrimeThanks for such a quick reponse&Prime'}/>
+            <Client title={'abgcompu'} desc={'&PrimeExcellent and fast. Will definitely hire again.&Prime'}/>
         </div>
 
         </>}/>
