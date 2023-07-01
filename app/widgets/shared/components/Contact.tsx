1dead7bf329a350751b0153722bd7c9bc3e56c04
Bilal <sheikhb885@gmail.com>
2023-07-02 02:42:15 +0500

    fixed bugs

--- app/widgets/shared/components/Contact.tsx
+++ app/widgets/shared/components/Contact.tsx
@@ -63,12 +63,12 @@ export default function Contact() {
         <section>
             <div id='Contact' className='bg-[#1a1a29] text-white py-10'>
                 <div>
-                    <div className='w-2/3 mx-auto text-3xl font-semibold text-[#7562E0]'>
+                    <div className='w-3/4 mx-auto text-3xl font-semibold text-[#7562E0]'>
                     Contact Me
                     </div>
                         {/* <TitleText title='Contact Me' textStyle='text-center' /> */}
-                        <form onSubmit={sendEmail} className='w-2/3 mx-auto'>
-                            <div className='flex flex-col md:w-2/3 w-full gap-6 my-4'>
+                        <form onSubmit={sendEmail} className='w-3/4 mx-auto'>
+                            <div className='flex flex-col md:w-3/4 w-full gap-6 my-4'>
                                 <div className='flex flex-col gap-2'>
                                     <label htmlFor="name" className='text-lg md:text-xl'>Enter your name:</label>
                                     <input required type="text" name="user_name" className='md:w-4/5 w-full h-10 bg-[#181730] border-2 border-[#EC994B] focus:border-3 focus:outline-none p-2 font-light rounded-lg focus:text-base] text-lg text-[#fdfaed]' />
@@ -83,7 +83,7 @@ export default function Contact() {
                                 </div>
                             </div>
                             <div className='flex flex-col mt-8 gap-4'>
-                                <label htmlFor="message" className='md:w-2/3 w-full text-lg md:text-xl'>Enter your message:</label>
+                                <label htmlFor="message" className='md:w-3/4 w-full text-lg md:text-xl'>Enter your message:</label>
                                 <textarea required name="message" className='md:w-4/5 w-full h-auto min-h-[8rem] lg:min-h-[10rem] overflow-visible bg-[#181730] border-2 border-[#EC994B] focus:border-3 focus:outline-none p-2 font-light rounded-lg text-lg text-[#fdfaed]' />
                             </div>
                             <div className={`flex items-center justify-center w-1/2 md:w-1/4 lg:w-2/12 my-8 mx-auto border-2 rounded-2xl border-[#555297] text-[#fdfaed] hover:border-none hover:bg-[#555297] cursor-pointer`}>
